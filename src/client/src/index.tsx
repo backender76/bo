fe4a670bb95d2090e24742994087b633eaa46ad2
alex <backender76@gmail.com>
2020-07-04 15:03:36 +0300

    Test pre-commit hook

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // call api example
-fetch('/api').then(res => res.json()).then(res => console.log(res));
+fetch('/api')
+  .then((res) => res.json())
+  .then((res) => console.log(res))
