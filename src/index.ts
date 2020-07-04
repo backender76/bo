32f84309ac0bcce012f350c261b4c1e915b2227d
alex <backender76@gmail.com>
2020-07-04 11:05:57 +0300

    Simple express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello ts-world');
+import * as express from 'express';
+import router from './routers/index';
+
+const port: number = 3700;
+
+const app = express();
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
